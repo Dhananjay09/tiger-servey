d45f21d0be5e5053a1bec4bacc4927f5573150d5
Dhananjay09 <dj091298@gmail.com>
2020-09-13 15:51:14 +0530

    Updates

---
+++ precache-manifest.deefa6d775e7dc332be52d6dc1a89043.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cd6a83a1260a8b9d284d0122aa68a49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "287b24a322e468cbce52",
+    "url": "/static/css/main.8a520d37.chunk.css"
+  },
+  {
+    "revision": "94603bbf556319f2c87e",
+    "url": "/static/js/2.4fc06429.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.4fc06429.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "287b24a322e468cbce52",
+    "url": "/static/js/main.aaa44015.chunk.js"
+  },
+  {
+    "revision": "b69862f621e48050774b",
+    "url": "/static/js/runtime-main.ab4ee6e1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
